476b89cdaf386d918f4aaf71a93247e4272b6d07
xieyanxieyan <13100887329@163.com>
2017-07-30 13:44:47 +0800

    VUE组件化开发

---
+++ HelloAugular_MVC.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/7/29.
+ */
+
+// var myModule = angular.module("HelloAngular",[]);
+// myModule.controller("helloAngular",['$scope',
+    function HelloAngular($scope){
+        $scope.greeting={
+            text:'hello'
+        };
+    }
+// ]);
